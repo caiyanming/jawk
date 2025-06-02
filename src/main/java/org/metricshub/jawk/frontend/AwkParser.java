d100060015ef8c6979ff31e254289ba606305be8
Bertrand Martin <bertrand@sentrysoftware.com>
2025-06-02 17:14:42 +0200

    Refined checkstyle to support uppercase method names in AwkParser.java

--- src/main/java/org/metricshub/jawk/frontend/AwkParser.java
+++ src/main/java/org/metricshub/jawk/frontend/AwkParser.java
@@ -940,7 +940,7 @@ public class AwkParser {
 	}
 
 	// RECURSIVE DECENT PARSER:
-	// CHECKSTYLE:OFF MethodName
+	// CHECKSTYLE.OFF: MethodName
 	// SCRIPT : \n [RULE_LIST] EOF
 	AST SCRIPT() throws IOException {
 		AST rl;
@@ -2116,7 +2116,7 @@ public class AwkParser {
 		return new ContinueStatement_AST();
 	}
 
-	// CHECKSTYLE:ON MethodName
+	// CHECKSTYLE.ON MethodName
 
 	private void expectKeyword(String keyword) throws IOException {
 		if (token == KEYWORDS.get(keyword)) {
